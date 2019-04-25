c2e553c922d455570d5abdbe3003ade3ffa40c1e
Chris Jackson <49789438+cdjackson14@users.noreply.github.com>
2019-04-24 23:19:54 -0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,90 +1,73 @@
 #!/bin/bash
 
-echo ''
-echo ''
-echo '################################################################################'
-echo '     Updating the .bashrc'
-echo '################################################################################'
+create-alias () {
+	# Update the .bashrc with some helpful alias commands
+	echo -e "
+	alias update='sudo apt update'
+	alias install='sudo apt install'
+	alias upgrade='sudo apt upgrade'
+	alias remove='sudo apt remove'
+	alias purge='sudo apt purge'
+	alias search='sudo apt search'
+	alias pp='ps -ef | grep '
+	alias h='history'
+	alias sshgat='ssh -C -p2222 g1e0x5r5@jackconsult.com'
+	alias sshgen='ssh -C jackchr1@genesis.local'
+	alias sshjackjack='ssh -C jackchr1@jackjack.duckdns.org'
+	" >> .bashrc &&
+	source ~/.bashrc
+}
 
-# Update the .bashrc with some helpful alias commands
-echo -e "
-alias update='sudo apt update'
-alias install='sudo apt install'
-alias upgrade='sudo apt upgrade'
-alias remove='sudo apt remove'
-alias purge='sudo apt purge'
-alias search='sudo apt search'
-alias pp='ps -ef | grep '
-alias h='history'
-alias sshgat='ssh -C -p2222 g1e0x5r5@jackconsult.com'
-alias sshgen='ssh -C jackchr1@genesis.local'
-alias sshjackjack='ssh -C jackchr1@jackjack.duckdns.org'
-" >> .bashrc &&
-source ~/.bashrc
+update-upgrade () {
+	# Update the system
+	sudo apt update &&
+	sudo apt upgrade -y
+}
 
-echo ''
-echo ''
-echo '################################################################################'
-echo '     Updating the core system'
-echo '################################################################################'
-# Update the system
-sudo apt update &&
-sudo apt upgrade -y
+build-essentials () {
+	# Install the basics for compiling and inside a VM
+	sudo apt install -y build-essential module-assistant dkms
+}
 
-echo ''
-echo ''
-echo '################################################################################'
-echo '     Install a bunch of basic goodies that I think we will want'
-echo '################################################################################'
-# Install the basics for compiling and inside a VM
-sudo apt install -y build-essential module-assistant dkms
+essentials () {
+	# Install the essential stuff for most all Debian based systems (Deb, Ubuntu, RaspberryPi, Kali...)
+	sudo apt install -y net-tools nmon ssh screen emacs sshfs cmatrix vlc mplayer rtorrent screen exiv2 
+}
 
-# Install the essential Ubuntu stuff and typical stuff
-sudo apt install -y net-tools nmon ssh screen ssh emacs sshfs iftop ircii rdesktop exiv2 mplayer rtorrent screen cmatrix vlc ubuntu-wallpapers* 
+optionals () {
+	sudo apt install -y rdesktop iftop ircii ubuntu-wallpapers* ubuntu-restricted-extras
+}
 
-echo ''
-echo ''
-echo '################################################################################'
-echo '     Installing Google Chrome web browser'
-echo '################################################################################'
-# Installation of Google Chrome
-if [[ $(getconf LONG_BIT) = "64" ]]
-then
-	echo "64bit Detected" &&
-	echo "Installing Google Chrome" &&
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
-	sudo dpkg -i google-chrome-stable_current_amd64.deb &&
-	sudo apt --fix-broken install
-	rm -f google-chrome-stable_current_amd64.deb
-else
-	echo "32bit Detected" &&
-	echo "Installing Google Chrome" &&
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb &&
-	sudo dpkg -i google-chrome-stable_current_i386.deb &&
-	sudo apt --fix-broken install
-	rm -f google-chrome-stable_current_i386.deb
-fi
+xfce () {
+	# Install the essential XFCE Ubuntu stuff
+	sudo apt install -y xubuntu-desktop xfce4 xfce4-goodies
+}
 
-echo ''
-echo ''
-echo '################################################################################'
-echo '     Installing XFCE/Ubuntu needed stuff'
-echo '################################################################################'
-# Install the essential XFCE Ubuntu stuff
-sudo apt install -y xubuntu-desktop xfce4 xfce4-goodies
+google-chrome () {
+	# Installation of Google Chrome
+	if [[ $(getconf LONG_BIT) = "64" ]]
+	then
+		echo "64bit Detected" &&
+		echo "Installing Google Chrome" &&
+		wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
+		sudo dpkg -i google-chrome-stable_current_amd64.deb &&
+		sudo apt --fix-broken install
+		rm -f google-chrome-stable_current_amd64.deb
+	else
+		echo "32bit Detected" &&
+		echo "Installing Google Chrome" &&
+		wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb &&
+		sudo dpkg -i google-chrome-stable_current_i386.deb &&
+		sudo apt --fix-broken install
+		rm -f google-chrome-stable_current_i386.deb
+	fi
+}
 
-
-echo ''
-echo ''
-echo '################################################################################'
-echo '     Installing Google Remote Desktop'
-echo '################################################################################'
-# Install the Google Remote Desktop
-# Visit https://remotedesktop.google.com/headless to register and get code to paste in to terminal later
-# Only ran if the option 'gremote' is added as a switch on the command line
-# https://remotedesktop.google.com/headless
-if [[ $1 = "gremote" ]]
-then
+google-remote () {
+	# Install the Google Remote Desktop
+	# Visit https://remotedesktop.google.com/headless to register and get code to paste in to terminal later
+	# Only ran if the option 'gremote' is added as a switch on the command line
+	# https://remotedesktop.google.com/headless
 	echo 'Installing the Google Chrome Remote software...'
 	wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 	sudo apt install -y --with-source=chrome-remote-desktop_current_amd64.deb chrome-remote-desktop
@@ -92,44 +75,42 @@ then
 	echo "xfce4-session" > ~/.chrome-remote-desktop-session
 	sudo apt install --assume-yes xscreensaver
 	sudo systemctl disable lightdm.service
-else
-	echo 'The "gremote" option was not specified, skipping install and config of Google Chrome Remote.'
-fi
-
-echo ''
-echo ''
-echo '################################################################################'
-echo '     Installing the restricted Ubuntu stff'
-echo '################################################################################'
-# Install extra, which will require user input, so putting at end of script
-sudo apt-get install -y ubuntu-restricted-extras
-
-echo ''
-echo ''
-echo '################################################################################'
-echo '     Setting up the SSH keys'
-echo '################################################################################'
-# Setup SSH which will require user input, so putting at end of script
-ssh-keygen
-
-echo ''
-echo ''
-echo '################################################################################'
-echo '     OK, we are finally done, not let us clean up...'
-echo '################################################################################'
-# Clean up everything
-echo "Cleaning Up" &&
-sudo apt -f install &&
-sudo apt autoremove &&
-sudo apt -y autoclean &&
-sudo apt -y clean
-
-if [[ $1 = "gremote" ]]
-then
 	echo ''
 	echo ''
-	echo ''
+	echo '######################################################################'
 	echo 'Hey... since you install the Google Remote, you will want to visit:'
-	echo ''
 	echo 'https://remotedesktop.google.com/headless'
-fi
+	echo '######################################################################'
+	echo ''
+	echo ''
+}
+
+
+ssh-config () {
+	# Setup SSH which will require user input, so putting at end of script
+	ssh-keygen
+}
+
+clean-up () {
+	# Clean up everything
+	echo "Cleaning Up" &&
+	sudo apt -f install &&
+	sudo apt autoremove &&
+	sudo apt -y autoclean &&
+	sudo apt -y clean
+}
+
+######################################################################
+# Here are the functions that will run.  
+# Simple remove what you do not want to execute.
+######################################################################
+create-alias
+update-upgrade
+build-essentials
+essentials
+optionals
+xfce
+google-chrome
+google-remote 
+ssh-config
+clean-up
