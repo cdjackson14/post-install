4b8bc44c2308485d712378fb270e5c479488198d
Chris Jackson <49789438+cdjackson14@users.noreply.github.com>
2019-06-20 17:57:38 -0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -172,6 +172,7 @@ lamp () {
 
 realtek-wifi () {
 	sudo apt install build-essential git libelf-dev dkms bc
+	#sudo apt install raspberrypi-kernel-headers raspberrypi-kernel
 	git clone https://github.com/cilynx/rtl88x2BU_WiFi_linux_v5.3.1_27678.20180430_COEX20180427-5959.git
 	cd rtl88x2BU_WiFi_linux_v5.3.1_27678.20180430_COEX20180427-5959
 	VER=$(sed -n 's/\PACKAGE_VERSION="\(.*\)"/\1/p' dkms.conf)
