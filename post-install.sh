bc4458b5331ce20459fb98af9c267cc6344445ba
Chris Jackson <49789438+cdjackson14@users.noreply.github.com>
2019-08-03 12:00:49 -0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -37,7 +37,7 @@ create-swap () {
 	sudo chmod 600 /swapfile
 	sudo mkswap /swapfile
 	sudo swapon /swapfile
-	sudo echo '/swapfile swap swap defaults 0 0' >> /etc/fstab
+	echo '/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab
 }
 
 update-upgrade () {
