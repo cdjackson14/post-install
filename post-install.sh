8b4302b6acc75c8e4bcb34f64160e5adfa881035
jackchr1 <cdjackson14@gmail.com>
2024-01-03 20:00:58 -0500

    changed brave install to auto Y

--- post-install.sh
+++ post-install.sh
@@ -156,7 +156,7 @@ brave-browser () {
 	sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 	echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 	sudo apt update
-	sudo apt -f install brave-browser
+	sudo apt install -y brave-browser
 
 	# Any message to display post all selected installs and configs.  Listed in a end summary.
 	POSTMSG[${COUNT}]="${FUNCNAME} "
